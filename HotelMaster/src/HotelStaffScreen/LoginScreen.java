443822b073edb129bedc3502b60d7c94f2d5eac3
mesutgurlek <mesutgurlekcs@gmail.com>
2015-11-22 17:57:42 +0200

    Screens

---
+++ HotelMaster/src/HotelStaffScreen/LoginScreen.java
@@ -0,0 +1,29 @@
+/*
+ * Created by JFormDesigner on Sun Nov 22 17:41:07 GMT+02:00 2015
+ */
+
+package HotelStaffScreen;
+
+import java.awt.*;
+import javax.swing.*;
+import com.jgoodies.forms.factories.*;
+import com.jgoodies.forms.layout.*;
+
+/**
+ * @author unknown
+ */
+public class LoginScreen extends JFrame {
+    public LoginScreen() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+        // Generated using JFormDesigner Evaluation license - Ugurcan Aytar
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    // Generated using JFormDesigner Evaluation license - Ugurcan Aytar
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
+}
