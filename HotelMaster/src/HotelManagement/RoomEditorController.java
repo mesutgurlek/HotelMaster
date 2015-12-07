fa2d7a33da57cf6297e6c7c6f0e43e6af9426e1f
mesutgurlek <mesutgurlekcs@gmail.com>
2015-12-07 08:26:32 +0200

    RoomEditorView Done

--- HotelMaster/src/HotelManagement/RoomEditorController.java
+++ HotelMaster/src/HotelManagement/RoomEditorController.java
@@ -1,6 +1,9 @@
 package HotelManagement;
 
 import HotelEntities.Hotel;
+import HotelEntities.Room;
+import HotelEntities.RoomStatus;
+import HotelEntities.RoomType;
 import HotelStaffScreen.Main;
 import HotelStaffScreen.RoomEditorView;
 
@@ -17,5 +20,34 @@ public class RoomEditorController {
             Hotel hotel = new Hotel();
             Main.changeSceneRoot(hotel.getMenuView());
         });
+
+        roomEditorView.getConfirmButton().setOnAction(e->{
+            int roomNo = Integer.parseInt(roomEditorView.getRoomNoField().getText());
+            int floorNo = Integer.parseInt(roomEditorView.getFloorNoField().getText());
+            double dailyPrice = Double.parseDouble(roomEditorView.getDailyPriceField().getText());
+            int adultCount = Integer.parseInt(roomEditorView.getAdultCountField().getText());
+            int childCount = Integer.parseInt(roomEditorView.getChildCountField().getText());
+            String extraInfo = roomEditorView.getExtraInfoField().getText();
+            RoomType roomType = typeConverter(roomEditorView.getRoomTypeBox().getValue().toString());
+
+            Room room = new Room(roomNo, floorNo, roomType, RoomStatus.empty, dailyPrice, childCount, adultCount, extraInfo);
+            RoomController roomController = new RoomController();
+            roomController.addRoom(room);
+        });
+    }
+
+    public RoomType typeConverter(String type){
+        if( type.equals("king")){
+            return RoomType.king;
+        }
+        else if( type.equals("family") ){
+            return RoomType.family;
+        }
+        else if( type.equals("regular") ){
+            return RoomType.regular;
+        }
+        else{
+            return null;
+        }
     }
 }
